631f0445a3d812e154892d4e5c43e0da93b80ead
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-09-24 11:28:34 -0300

    app com comentarios

---
+++ app.js
@@ -0,0 +1,11 @@
+/***************************************************************************************************************************
+ * Objetivo: EndPoints referente a API de contatos e conversas.
+ * Data: 24/09/2025
+ * Autor: Guilherme Moreira
+ * Versão: 1.0
+ * 
+ * Observações: Instalação do Express, Cors, Body-Parser
+ * npm install express     --save
+ * npm install cors        --save
+ * npm install body-parser --save
+ ***************************************************************************************************************************/
