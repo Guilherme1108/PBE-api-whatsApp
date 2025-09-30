b2736538cbba815ac02c9ab10b4db47ec78133c6
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-09-30 10:46:59 -0300

    todas as funções criadas e funcionando

--- app.js
+++ app.js
@@ -9,3 +9,77 @@
  * npm install cors        --save
  * npm install body-parser --save
  ***************************************************************************************************************************/
+
+//Import das dependencias da API
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+//import das funcoes
+const dados = require('./modules/funcoes.js')
+
+//porta
+const PORT = process.PORT || 8080
+
+const app = express()
+
+//permissões
+app.use((request, response, next) => {
+    response.header('Acces-Control-Allow-Origin', '*')    //Servidor de origem da API
+    response.header('Acces-Control-Allow-Methods', 'GET') //verbos permitidos / para adicionar mais métodos separar por , dentro da mesma aspas
+    //carrega as configurações no cors da API 
+    app.use(cors())
+    next() // proximo, carregar os proximos endpoints
+})
+
+//endpoints
+app.get('/v1/dados/', function (request, response) {
+    let allDados = dados.getAllDados()
+    response.status(allDados.status_code)
+    response.json(allDados)
+})
+
+app.get('/v1/userdados/:number', function (request, response) {
+    let number = request.params.number
+    let userDados = dados.getAllUserDados(number)
+    response.status(userDados.status_code)
+    response.json(userDados)
+})
+
+app.get('/v1/contacts/:number', function (request, response) {
+    let number = request.params.number
+    let userDados = dados.getContactsFromUser(number)
+    response.status(userDados.status_code)
+    response.json(userDados)
+})
+
+app.get('/v1/user/messages/:number', function (request, response) {
+    let number = request.params.number
+    let userMessages = dados.getAllUserMessages(number)
+    response.status(userMessages.status_code)
+    response.json(userMessages)
+})
+
+app.get('/v1/user/messages', function (request, response) {
+    let userNumber = request.query.userNumber
+    let contactNumber = request.query.contactNumber
+    let userMessages = dados.getConversation(userNumber, contactNumber)
+    response.status(userMessages.status_code)
+    response.json(userMessages)
+})
+
+app.get('/v1/messages', function (request, response) {
+    let userNumber = request.query.userNumber
+    let contactNumber = request.query.contactNumber
+    let keyWord = request.query.keyWord
+    let userMessages = dados.getKeywordsFromConversation(userNumber, contactNumber, keyWord)
+    response.status(userMessages.status_code)
+    response.json(userMessages)
+})
+
+
+
+//Start na API
+app.listen(PORT, function () {
+    console.log('API aguardando requisições ....')
+})
--- modules/funcoes.js
+++ modules/funcoes.js
@@ -29,13 +29,14 @@ const getAllDados = function () {
 // console.log(teste.allDados[0].contacts)
 
 const getAllUserDados = function (number) {
+
     let message = { status: true, status_code: 200, development: 'Guilherme Moreira', userDados: [] }
 
     // o filter vai procurar em todos os usuarios algum com este numero e devovler tudo dele
     const userFind = dados.contatos['whats-users'].filter(user => user.number === number)
 
     if (userFind.length > 0) {
-        message.userDados = encontrados
+        message.userDados = userFind
         return message // 200
     } else {
         return MESSAGE_ERRO
@@ -43,38 +44,40 @@ const getAllUserDados = function (number) {
 }
 
 
-const getPersonalUserDados = function (number){
-    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', personalDados: [] 
+const getContactsFromUser = function (number) {
+    let message = {
+        status: true, status_code: 200, development: 'Guilherme Moreira', contacts: []
     }
 
-    
     const userFind = dados.contatos['whats-users'].find(user => user.number === number)
 
     if (userFind) {
-        const user = userFind.contacts
-
-        message.personalDados = {
-            name: user.name,
-            description: user.description,
-            image: user.image
-        }
+        message.contacts = userFind.contacts.map(contact => ({ //o map pega o contato e cria um objeto novo só com as informações que eu quero
+            name: contact.name,
+            description: contact.description,
+            image: contact.image
+        }))
         return message
     } else {
         return MESSAGE_ERRO
     }
 }
 
-console.log(getPersonalUserDados('11987876567'))
 
-const getAllPersonalDados = function (number) {
+const getAllUserMessages = function (number) {
     //Variavel de base para o cabeçalho da API
-    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', allPersonalDados: [] }
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', allMessages: [] }
 
     const userFind = dados.contatos['whats-users'].find(user => user.number === number)
 
     if (userFind) {
-        const user = userFind.contacts[0]
-        message.allPersonalDados.push(user)
+        // Percorre todos os contatos do usuário
+        userFind.contacts.forEach(contact => {
+            // Para cada contato, pega todas as mensagens
+            contact.messages.forEach(mensagem => {
+                message.allMessages.push(mensagem)
+            })
+        })
 
         return message
     } else {
@@ -82,13 +85,71 @@ const getAllPersonalDados = function (number) {
     }
 }
 
-//console.log(getAllPersonalDados('11987876567'))
+const getConversation = function (userNumber, contactNumber) {
+    //Variavel de base para o cabeçalho da API
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', contactName: '', contactNumber: '', conversation: [] }
 
+    const userFind = dados.contatos['whats-users'].find(user => user.number === userNumber)
 
+    if (userFind) {
 
+        const contactfind = userFind.contacts.find(contact => contact.number === contactNumber)
+        message.contactName = contactfind.name,
+        message.contactNumber = contactfind.number
+
+        if (contactfind) {
+            contactfind.messages.forEach(mensagem => {
+                message.conversation.push(mensagem)
+            })
+            return message
+
+        } else {
+            return MESSAGE_ERRO
+        }
+
+    } else {
+        return MESSAGE_ERRO
+    }
+}
+
+ const getKeywordsFromConversation = function (userNumber, contactNumber, keyWord) {
+    //Variavel de base para o cabeçalho da API
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', conversation: [] }
+
+    const userFind = dados.contatos['whats-users'].find(user => user.number === userNumber)
+
+    if (userFind) {
+
+        const contactfind = userFind.contacts.find(contact => contact.number === contactNumber)
+
+        if (contactfind) {
+            contactfind.messages.forEach(mensagem => {
+                
+                if (mensagem.content.toLowerCase().includes(keyWord.toLowerCase())) {
+                    message.conversation.push(mensagem)
+                    return message
+                } else {
+                    return MESSAGE_ERRO
+                }
+
+            })
+            return message
+
+        } else {
+            return MESSAGE_ERRO
+        }
+
+    } else {
+        return MESSAGE_ERRO
+    }
+}
+// console.log(getKeywordsFromConversation('11987876567', '26999999963', 'how'))
 
 module.exports = {
     getAllDados,
     getAllUserDados,
-    getPersonalUserDados
+    getContactsFromUser,
+    getAllUserMessages,
+    getConversation,
+    getKeywordsFromConversation
 }
